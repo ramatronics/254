04f9f27b95f2cbc24af6ca5c344b6b8b0989d54d
sh6khan <sadman.khan999@gmail.com>
2015-07-03 00:25:25 -0400

    added more needed libraries

--- lab4/lab4_source_code/producer.c
+++ lab4/lab4_source_code/producer.c
@@ -1,11 +1,14 @@
-#include <stdbool.h>
-#include <string.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h>
 #include <mqueue.h>
 #include <sys/stat.h>
-#include <signal.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <errno.h>
+#include <sys/time.h>
+#include <time.h>
+#include <string.h>
 
 
 
